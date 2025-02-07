ee605298a409bafa1683db42ded6411256039838
Ash-log-coding <gabrielledonati023@gmail.com>
2025-02-07 09:06:06 +0100

    surrpimer l'import use effect du fichier tsx dashboard

--- src/MainView/5-panels/Dashboard/Dashboard.tsx
+++ src/MainView/5-panels/Dashboard/Dashboard.tsx
@@ -1,9 +1,9 @@
 //********** Imports **********//
 import { Box, CssBaseline } from "@mui/material";
+import { useState } from "react";
 import AppBar from "../../4-templates/AppBar/AppBar";
-import { useEffect, useState } from "react";
-import LeftMenu from "../../4-templates/LeftMenu";
 import DashboardContent from "../../4-templates/DashboardContent";
+import LeftMenu from "../../4-templates/LeftMenu";
 
 //********** Component **********//
 export const Dashboard = () => {
